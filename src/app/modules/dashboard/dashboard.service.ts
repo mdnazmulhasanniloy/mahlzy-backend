72b5ecec77c078fe7a4cb515a5bff5c1bb5a10a0
mdnazmulhasanniloy <mdnazmulhasanniloy323@gmail.com>
2025-11-11 11:40:07 +0600

    [Done] dashboard apis

--- src/app/modules/dashboard/dashboard.service.ts
+++ src/app/modules/dashboard/dashboard.service.ts
@@ -5,6 +5,9 @@ import moment from 'moment';
 import { initializeMonthlyData } from './dashboard.utils';
 import { MonthlyIncome, monthlyOrders } from './dashboard.interface';
 import Products from '../products/products.models';
+import { User } from '../user/user.models';
+import { USER_ROLE } from '../user/user.constants';
+import Shop from '../shop/shop.models';
 
 const resturantDashboardTopCard = async (userId: string) => {
   const orderStats = await Orders.aggregate([
@@ -47,7 +50,7 @@ const resturantDashboardTopCard = async (userId: string) => {
           {
             $group: {
               _id: null,
-              total: { $sum: { $multiply: ['$totalPrice', 0.95] } },
+              total: { $sum: '$resturantPercentage' },
             },
           },
         ],
@@ -62,7 +65,7 @@ const resturantDashboardTopCard = async (userId: string) => {
           {
             $group: {
               _id: null,
-              total: { $sum: { $multiply: ['$totalPrice', 0.9] } },
+              total: { $sum: '$resturantPercentage' },
             },
           },
         ],
@@ -78,29 +81,29 @@ const resturantDashboardTopCard = async (userId: string) => {
             },
           },
 
-          {
-            $addFields: {
-              calculatedIncome: {
-                $cond: {
-                  if: {
-                    $in: ['$paymentStatus', ['cash_on_delivery', 'pickup']],
-                  },
-                  then: { $multiply: ['$totalPrice', 0.95] },
-                  else: {
-                    $cond: {
-                      if: { $eq: ['$paymentStatus', 'paid'] },
-                      then: { $multiply: ['$totalPrice', 0.9] },
-                      else: 0,
-                    },
-                  },
-                },
-              },
-            },
-          },
+          // {
+          //   $addFields: {
+          //     calculatedIncome: {
+          //       $cond: {
+          //         if: {
+          //           $in: ['$paymentStatus', ['cash_on_delivery', 'pickup']],
+          //         },
+          //         then: { $multiply: ['$totalPrice', 0.95] },
+          //         else: {
+          //           $cond: {
+          //             if: { $eq: ['$paymentStatus', 'paid'] },
+          //             then: { $multiply: ['$totalPrice', 0.9] },
+          //             else: 0,
+          //           },
+          //         },
+          //       },
+          //     },
+          //   },
+          // },
           {
             $group: {
               _id: null,
-              total: { $sum: '$calculatedIncome' },
+              total: { $sum: '$resturantPercentage' },
             },
           },
         ],
@@ -115,29 +118,29 @@ const resturantDashboardTopCard = async (userId: string) => {
             },
           },
 
-          {
-            $addFields: {
-              calculatedIncome: {
-                $cond: {
-                  if: {
-                    $in: ['$paymentStatus', ['cash_on_delivery', 'pickup']],
-                  },
-                  then: { $multiply: ['$totalPrice', 0.95] },
-                  else: {
-                    $cond: {
-                      if: { $eq: ['$paymentStatus', 'paid'] },
-                      then: { $multiply: ['$totalPrice', 0.9] },
-                      else: 0,
-                    },
-                  },
-                },
-              },
-            },
-          },
+          // {
+          //   $addFields: {
+          //     calculatedIncome: {
+          //       $cond: {
+          //         if: {
+          //           $in: ['$paymentStatus', ['cash_on_delivery', 'pickup']],
+          //         },
+          //         then: { $multiply: ['$totalPrice', 0.95] },
+          //         else: {
+          //           $cond: {
+          //             if: { $eq: ['$paymentStatus', 'paid'] },
+          //             then: { $multiply: ['$totalPrice', 0.9] },
+          //             else: 0,
+          //           },
+          //         },
+          //       },
+          //     },
+          //   },
+          // },
           {
             $group: {
               _id: null,
-              total: { $sum: '$calculatedIncome' },
+              total: { $sum: '$resturantPercentage' },
             },
           },
         ],
@@ -149,29 +152,29 @@ const resturantDashboardTopCard = async (userId: string) => {
             },
           },
 
-          {
-            $addFields: {
-              calculatedIncome: {
-                $cond: {
-                  if: {
-                    $in: ['$paymentStatus', ['cash_on_delivery', 'pickup']],
-                  },
-                  then: { $multiply: ['$totalPrice', 0.95] },
-                  else: {
-                    $cond: {
-                      if: { $eq: ['$paymentStatus', 'paid'] },
-                      then: { $multiply: ['$totalPrice', 0.9] },
-                      else: 0,
-                    },
-                  },
-                },
-              },
-            },
-          },
+          // {
+          //   $addFields: {
+          //     calculatedIncome: {
+          //       $cond: {
+          //         if: {
+          //           $in: ['$paymentStatus', ['cash_on_delivery', 'pickup']],
+          //         },
+          //         then: { $multiply: ['$totalPrice', 0.95] },
+          //         else: {
+          //           $cond: {
+          //             if: { $eq: ['$paymentStatus', 'paid'] },
+          //             then: { $multiply: ['$totalPrice', 0.9] },
+          //             else: 0,
+          //           },
+          //         },
+          //       },
+          //     },
+          //   },
+          // },
           {
             $group: {
               _id: null,
-              total: { $sum: '$calculatedIncome' },
+              total: { $sum: '$resturantPercentage' },
             },
           },
         ],
@@ -237,31 +240,31 @@ const resturantDashboardChart = async (query: Record<string, any>) => {
           },
 
           // 👇 Add this stage to calculate income based on paymentStatus
-          {
-            $addFields: {
-              calculatedIncome: {
-                $cond: {
-                  if: {
-                    $in: ['$paymentStatus', ['cash_on_delivery', 'pickup']],
-                  },
-                  then: { $multiply: ['$totalPrice', 0.95] },
-                  else: {
-                    $cond: {
-                      if: { $eq: ['$paymentStatus', 'paid'] },
-                      then: { $multiply: ['$totalPrice', 0.9] },
-                      else: 0,
-                    },
-                  },
-                },
-              },
-            },
-          },
+          // {
+          //   $addFields: {
+          //     calculatedIncome: {
+          //       $cond: {
+          //         if: {
+          //           $in: ['$paymentStatus', ['cash_on_delivery', 'pickup']],
+          //         },
+          //         then: { $multiply: ['$totalPrice', 0.95] },
+          //         else: {
+          //           $cond: {
+          //             if: { $eq: ['$paymentStatus', 'paid'] },
+          //             then: { $multiply: ['$totalPrice', 0.9] },
+          //             else: 0,
+          //           },
+          //         },
+          //       },
+          //     },
+          //   },
+          // },
 
           // 👇 Group by month and sum the calculated income
           {
             $group: {
               _id: { month: { $month: '$createdAt' } },
-              income: { $sum: '$calculatedIncome' },
+              income: { $sum: '$resturantPercentage' },
             },
           },
 
@@ -284,7 +287,6 @@ const resturantDashboardChart = async (query: Record<string, any>) => {
     },
   );
 
-  
   const monthlyOrders = initializeMonthlyData('total') as monthlyOrders[];
   orderStatus.monthlyOrders.forEach(
     ({ _id, total }: { _id: { month: number }; total: number }) => {
@@ -343,7 +345,6 @@ const resturantCustomerList = async (query: Record<string, any>) => {
     { $unwind: '$user' },
   ];
 
- 
   if (query?.searchTerm) {
     pipeline.push({
       $match: {
@@ -357,7 +358,6 @@ const resturantCustomerList = async (query: Record<string, any>) => {
     });
   }
 
-  
   pipeline.push({
     $facet: {
       totalCount: [{ $count: 'uniqueCustomers' }],
@@ -380,7 +380,6 @@ const resturantCustomerList = async (query: Record<string, any>) => {
     },
   });
 
-   
   pipeline.push({
     $project: {
       uniqueCustomers: {
@@ -393,11 +392,321 @@ const resturantCustomerList = async (query: Record<string, any>) => {
   const result = await Orders.aggregate(pipeline);
   return {
     data: result[0].users || [],
-    meta:{
+    meta: {
       total: result[0].uniqueCustomers || 0,
       page,
       limit,
-    }
+    },
+  };
+};
+
+const adminDashboardTopCard = async () => {
+  const orderStats = await Orders.aggregate([
+    {
+      $match: {
+        isDeleted: false,
+        paymentStatus: { $in: ['paid', 'cash_on_delivery', 'pickup'] },
+      },
+    },
+    {
+      $facet: {
+        totalOrders: [
+          {
+            $group: {
+              _id: null,
+              total: { $sum: 1 },
+            },
+          },
+        ],
+        pickupIncome: [
+          {
+            $match: {
+              status: ORDER_STATUS.delivered,
+              paymentStatus: { $in: ['cash_on_delivery', 'pickup'] },
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              total: { $sum: '$adminPercentage' },
+            },
+          },
+        ],
+        onlineIncome: [
+          {
+            $match: {
+              status: ORDER_STATUS.delivered,
+              paymentStatus: 'paid',
+            },
+          },
+
+          {
+            $group: {
+              _id: null,
+              total: { $sum: '$adminPercentage' },
+            },
+          },
+        ],
+
+        totalIncome: [
+          {
+            $match: {
+              status: ORDER_STATUS.delivered,
+              paymentStatus: { $in: ['paid', 'cash_on_delivery', 'pickup'] },
+            },
+          },
+
+          {
+            $group: {
+              _id: null,
+              total: { $sum: '$adminPercentage' },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  const totalUsers = await User.countDocuments({
+    role: USER_ROLE.user,
+    isDeleted: false,
+  });
+  const totalResturant = await Shop.countDocuments({
+    isDeleted: false,
+  });
+  const stats = orderStats[0];
+  const totalOrders = stats.totalOrders[0]?.total || 0;
+  const totalIncome = stats.totalIncome[0]?.total || 0;
+
+  return {
+    totalUsers,
+    totalResturant,
+    totalOrders,
+    totalIncome,
+  };
+};
+
+const adminDashboardChart = async (query: Record<string, any>) => {
+  const incomeYear = query.incomeYear || moment().year();
+  const orderYear = query.orderYear || moment().year();
+
+  const orderStatus = await Orders.aggregate([
+    {
+      $facet: {
+        topOrders: [
+          {
+            $match: {
+              isDeleted: false,
+              status: { $ne: 'canceled' },
+              createdAt: {
+                $gte: moment().year(orderYear).startOf('year').toDate(),
+                $lte: moment().year(orderYear).endOf('year').toDate(),
+              },
+            },
+          },
+
+          {
+            $group: {
+              _id: '$author',
+              totalSales: { $sum: '$totalPrice' },
+              totalOrders: { $sum: 1 },
+            },
+          },
+          {
+            $lookup: {
+              from: 'shops',
+              localField: '_id',
+              foreignField: 'author',
+              as: 'shop',
+            },
+          },
+
+          { $unwind: '$shop' },
+          {
+            $project: {
+              _id: 0,
+              authorId: '$_id',
+              totalSales: 1,
+              totalOrders: 1,
+              'author.name': 1,
+              'author.email': 1,
+              'author.profile': 1,
+              'shop.shopName': 1,
+              'shop.profile': 1,
+              'shop.banner': 1,
+              'shop.bannerColor': 1,
+            },
+          },
+          {
+            $sort: { totalSales: -1 },
+          },
+          {
+            $limit: 5,
+          },
+        ],
+
+        monthlyIncome: [
+          {
+            $match: {
+              paymentStatus: { $in: ['paid', 'cash_on_delivery', 'pickup'] },
+              status: ORDER_STATUS.delivered,
+              isDeleted: false,
+              createdAt: {
+                $gte: moment().year(incomeYear).startOf('year').toDate(),
+                $lte: moment().year(incomeYear).endOf('year').toDate(),
+              },
+            },
+          },
+
+          // 👇 Add this stage to calculate income based on paymentStatus
+          {
+            $addFields: {
+              calculatedIncome: {
+                $cond: {
+                  if: {
+                    $in: ['$paymentStatus', ['cash_on_delivery', 'pickup']],
+                  },
+                  then: { $multiply: ['$totalPrice', 0.05] },
+                  else: {
+                    $cond: {
+                      if: { $eq: ['$paymentStatus', 'paid'] },
+                      then: { $multiply: ['$totalPrice', 0.1] },
+                      else: 0,
+                    },
+                  },
+                },
+              },
+            },
+          },
+
+          // 👇 Group by month and sum the calculated income
+          {
+            $group: {
+              _id: { month: { $month: '$createdAt' } },
+              income: { $sum: '$calculatedIncome' },
+            },
+          },
+
+          // {
+          //   $group: {
+          //     _id: { month: { $month: '$createdAt' } },
+          //     income: { $sum: '$hotelOwnerAmount' },
+          //   },
+          // },
+          // { $sort: { '_id.month': 1 } },
+        ],
+
+        recentlyPlacedOrder: [
+          {
+            $match: {
+              paymentStatus: { $in: ['paid', 'cash_on_delivery', 'pickup'] },
+              // status: ORDER_STATUS.delivered,
+              isDeleted: false,
+            },
+          },
+          {
+            $sort: { createdAt: -1 },
+          },
+          {
+            $limit: 5,
+          },
+          // Populate user
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'user',
+              foreignField: '_id',
+              as: 'user',
+              pipeline: [
+                {
+                  $project: {
+                    name: 1,
+                    email: 1,
+                    profile: 1,
+                    phoneNumber: 1,
+                  },
+                },
+              ],
+            },
+          },
+          { $unwind: '$user' },
+
+          // Populate author
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'author',
+              foreignField: '_id',
+              as: 'author',
+              pipeline: [
+                {
+                  $project: {
+                    name: 1,
+                    email: 1,
+                    profile: 1,
+                    phoneNumber: 1,
+                  },
+                },
+              ],
+            },
+          },
+          { $unwind: '$author' },
+
+          // Populate orderItems.product
+          {
+            $lookup: {
+              from: 'products',
+              localField: 'orderItems.product',
+              foreignField: '_id',
+              as: 'orderProducts',
+              pipeline: [
+                {
+                  $project: {
+                    name: 1,
+                    price: 1,
+                    image: 1,
+                    category: 1,
+                  },
+                },
+              ],
+            },
+          },
+
+          // Populate additionalItems.topping
+          {
+            $lookup: {
+              from: 'toppings',
+              localField: 'additionalItems.topping',
+              foreignField: '_id',
+              as: 'toppingItems',
+              pipeline: [
+                {
+                  $project: {
+                    name: 1,
+                    price: 1,
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ]).then(data => data[0]);
+
+  const monthlyIncome = initializeMonthlyData('income') as MonthlyIncome[];
+  orderStatus.monthlyIncome.forEach(
+    ({ _id, income }: { _id: { month: number }; income: number }) => {
+      monthlyIncome[_id.month - 1].income = Math.round(income);
+    },
+  );
+
+  const topOrders = orderStatus.topOrders || [];
+  const recentlyPlacedOrder = orderStatus.recentlyPlacedOrder || [];
+
+  return {
+    monthlyIncome,
+    topOrders,
+    recentlyPlacedOrder,
   };
 };
 
@@ -406,4 +715,6 @@ export const dashboardService = {
   resturantDashboardChart,
   resturantDashboardTables,
   resturantCustomerList,
+  adminDashboardTopCard,
+  adminDashboardChart,
 };
